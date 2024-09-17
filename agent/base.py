8aa3e1d532e11be6aaedbc7a2a7330e23f160f30
Eng Acs <me@engacs.pw>
2024-09-17 13:27:58 +0300

    Revert "Try to fixed"
    This reverts commit 7436a46933a067fd0cb212c48c9c252817395725.

--- agent/base.py
+++ agent/base.py
@@ -92,22 +92,11 @@ class Base:
             shell=True,
             executable=executable,
         ) as process:
-            # if input:
-            #     process._stdin_write(input.encode())
             if input:
-                try:
-                    process.stdin.write(input.encode())
-                    process.stdin.close()  # Ensure input stream is closed after writing
-                except BrokenPipeError:
-                    # Handle broken pipe error
-                    self.data.update({"status": "Failure", "traceback": "BrokenPipeError occurred"})
-                    self.log()
-                    raise
-                
+                process._stdin_write(input.encode())
 
             output = self.parse_output(process)
-            returncode = process.wait()  # Wait for the process to complete
-            #returncode = process.poll() or 0
+            returncode = process.poll() or 0
             # This is equivalent of check=True
             # Raise an exception if the process returns a non-zero return code
             if non_zero_throw and returncode:
